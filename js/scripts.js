3bd6729d02389a8ce772df13667aa62e30ba036d
afederwisch <86984837+afederwisch@users.noreply.github.com>
2022-04-16 21:16:58 -0700

    added registry page and countdown

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,19 @@ function openNav () {
         x.className = "nav";
     }
 }
+
+// Countdown timer
+let weddingDate = new Date("Oct 28, 2022 18:00:00").getTime();
+let x = setInterval(function() {
+    let now = new Date().getTime();
+    let distance = weddingDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.getElementById("timer").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "We Are Married!";
+      }
+    }, 1000);
